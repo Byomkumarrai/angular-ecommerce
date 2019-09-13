67cbc00d3bf06427b53c592d289094784ec2a741
Byomkumarrai <byomsuper@gmail.com>
2019-09-13 16:01:00 +0530

    cart,shipping and routing

---
+++ src/app/product.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: 'Phone Xl',
+    price: 900,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 1000,
+    description: 'it is standard phone number'
+  }
+];
